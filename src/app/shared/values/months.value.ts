f4c28dc087c345834d8a608e995288aedd5cc6cb
Jesse <jesse.matherne@esosolutions.com>
2017-09-07 15:07:45 -0500

    exported values for months of the year and weekday names

---
+++ src/app/shared/values/months.value.ts
@@ -0,0 +1,14 @@
+export const Months = [
+  { month: 'January', numDays: 31 },
+  { month: 'February', numDays: 28 },
+  { month: 'March', numDays: 31 },
+  { month: 'April', numDays: 30 },
+  { month: 'May', numDays: 31 },
+  { month: 'June', numDays: 30 },
+  { month: 'July', numDays: 31 },
+  { month: 'August', numDays: 31 },
+  { month: 'September', numDays: 30 },
+  { month: 'October', numDays: 31 },
+  { month: 'November', numDays: 30 },
+  { month: 'December', numDays: 31 }
+];
