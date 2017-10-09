d06aaa4206d50991c6b764874ab608904464e74e
Jesse <jessephilip@gmail.com>
2017-10-09 13:10:46 -0500

    hooked up functionality to signIn and register buttons

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { ComponentAnchorDirective } from './../../directives/component-anchor.directive';
+import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { RegisterComponent } from './../../components/register/register.component';
 
 @Component({
+  entryComponents: [
+    RegisterComponent
+  ],
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  @ViewChild(ComponentAnchorDirective) componentAnchor: ComponentAnchorDirective;
 
-  constructor () {}
+  constructor (private cfr: ComponentFactoryResolver) {}
   ngOnInit () {}
+
+  public signUp (): void {
+    // show register modal
+    const componentFactory = this.cfr.resolveComponentFactory(RegisterComponent);
+    const vcr = this.componentAnchor.viewContainerRef;
+    vcr.clear();
+    const componentRef = vcr.createComponent(componentFactory);
+  }
+
+  public login (): void {
+    alert('login');
+  }
 }
